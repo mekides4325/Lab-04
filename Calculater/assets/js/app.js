726f0beb5683698bb7c04362bc40f10674f4ada5
Mekides <se.mekides.bishaw@gmail.com>
2021-01-24 21:44:44 -0700

    an init Immediately Invoking function expression(IIFEs) were created in calculater app.js file folder.

--- Calculater/assets/js/app.js
+++ Calculater/assets/js/app.js
@@ -61,3 +61,84 @@ function avrage(arr){
 
 return av
 }
+
+//Immediately Invoking function expression(IIFEs)
+(function(){
+    let a;
+    let b;
+    let service = parseInt(prompt("choice: \n 1: Add  \n 2: Subtract  \n 3: multiply  \n 4:  Divide   \n 5: multiply multiple numbers \n 6: add multiple numbers \n 7:square \n 8:max \n 9:min \n 10:calculating avarage"));
+    switch (service) {
+        case 1:
+            alert(add2(parseInt(prompt("Enter first Number: ")),parseInt(prompt("Enter second Number: "))))
+            break;
+        case 2:
+                alert(sub(parseInt(prompt("Enter first Number: ")) ,parseInt(prompt("Enter second Number: "))))
+                break;
+
+        case 3:
+            alert(mult(parseInt(prompt("Enter first Number: ")),parseInt(prompt("Enter second Number: "))))
+            break; 
+            
+            
+            
+        case 4:
+                let a = prompt("Enter first Number: ") 
+                let b = prompt("Enter second Number: ")
+                if (b == 0){alert("division by zero is not supported")}
+                else{
+                    alert(div(a,b))
+                }
+                break;    
+      
+ 
+        case 5:
+            var nums = prompt("how many numbers tha you want to multiply: ")
+            array = new Array()
+            for(let i=0; i<nums; i++){
+                array[i] = parseInt(prompt("Enter Number("+(i+1)+"): "));
+            }
+            alert(multiplyMultiple(array))
+            break;
+        
+            case 6:
+                let length = prompt("how many number that you want to add : ")
+                array = new Array()
+                for(let i=0; i<length; i++){
+                    array[i] = parseInt(prompt("Enter Number("+(i+1)+"): "));
+                } 
+                alert(addMultiple(array))
+                break;
+
+
+            case 7:
+                alert(square(parseInt(prompt("Enter Number: "))))
+                alert(square(a))
+                break;
+            case 8:
+                alert(max(parseInt(prompt("Enter first Number: ")),parseInt(prompt("Enter second Number: "))))
+                alert(max(a,b))
+                break;
+
+            case 9:
+                alert(min(parseInt(prompt("Enter first Number: ")),parseInt(prompt("Enter second Number: "))))
+                alert(min(a,b))
+                break;
+         
+
+
+
+            case 10:
+                var nums = prompt("how many numbers that you want to calculate there avarage: ")
+                array = new Array()
+                for(let i=0; i<nums; i++){
+                    array[i] = parseInt(prompt("Enter Number("+(i+1)+"): "));
+                }
+                alert(avrage(array))
+                break;    
+           
+            default:
+            alert("Invalid input")
+            break;
+    }
+
+})();
